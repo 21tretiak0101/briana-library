8c1672cd858a1b16522cfef1d10c91ff37a6cc4d
ttre16 <ilia.tretiak.2001@mail.ru>
2020-03-24 01:19:35 +0300

    static page component added

---
+++ library-client/src/app/static-page/static-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {ContentService} from '../services/content.service';
+import {ActivatedRoute} from '@angular/router';
+import {ImageEditorService} from '../services/image-editor.service';
+
+@Component({
+  selector: 'app-static-page',
+  template: `
+    <div class="container text-justify mt-3" *ngIf="pageContent">
+      <div class="row">
+        <div class="col-md-8 col-md-12">
+          <div [innerHTML]="pageContent | safeHtml"></div>
+        </div>
+      </div>
+    </div>
+  `
+})
+export class StaticPageComponent implements OnInit {
+
+  public pageContent: string = '';
+
+  constructor(private contentService: ContentService,
+              private route: ActivatedRoute,
+              private imageEditorService: ImageEditorService) { }
+
+  ngOnInit(): void {
+    this.route.url.subscribe( value => {
+      this.getStaticContentByPageName(value[0].path);
+      console.log(value[0].path);
+    })
+  }
+
+  getStaticContentByPageName(pageName: string): void {
+    this.contentService.getStaticContentByPageName(pageName).subscribe( content => {
+      this.pageContent = this.imageEditorService.replaceImages(content);
+    })
+  }
+}
